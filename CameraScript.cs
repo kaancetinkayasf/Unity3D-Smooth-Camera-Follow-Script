d8fe00d2fe5f6a051346199da2273e44f7fa91f4
Berke Kaan Çetinkaya <46684966+kaancetinkayasf@users.noreply.github.com>
2020-05-16 00:41:19 +0300

    Added Camera Script

---
+++ CameraScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public Transform player;
+    public float smoothSpeed = 100f;
+
+    public Vector3 offset;
+    public Vector3 SmoothPosition;
+    
+
+    private void Start()
+    {
+        
+
+
+
+           // offset = Vector3.Lerp(1.4f,1.94f,-6.74f);
+    }
+
+
+     void FixedUpdate()
+    {
+            
+        offset = new Vector3(1.4f, 1.94f, -6.74f);
+        Vector3 AdjustedPosition = offset + player.position;
+        SmoothPosition = Vector3.Lerp(transform.position, AdjustedPosition, smoothSpeed * Time.deltaTime);
+        transform.position = SmoothPosition;
+
+       
+    }
+
+
+}
