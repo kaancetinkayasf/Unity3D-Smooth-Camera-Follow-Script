725f4f84dc9d446837bd034fcdabec014ac89c0f
Berke Kaan Çetinkaya <46684966+kaancetinkayasf@users.noreply.github.com>
2020-05-16 00:44:57 +0300

    Added Comments

--- CameraScript.cs
+++ CameraScript.cs
@@ -25,9 +25,16 @@ public class CameraScript : MonoBehaviour
      void FixedUpdate()
     {
             
+        // OFFSET FROM YOUR CAMERA
         offset = new Vector3(1.4f, 1.94f, -6.74f);
+
+        // FINAL CAMERA POSITION
         Vector3 AdjustedPosition = offset + player.position;
+
+        // THE TRANSFORM WHERE OUR CAMERA WILL MOVE FROM POSITION A TO B FOR OUR EXAMPLE IT WILL MOVE FROM "transform.position" to "AdjustedPosition"
         SmoothPosition = Vector3.Lerp(transform.position, AdjustedPosition, smoothSpeed * Time.deltaTime);
+
+        // SET THE CAMERA'S POSITION TO "SmoothPosition"
         transform.position = SmoothPosition;
 
        
